f38cd48d4ce76c6f6feaadda7cab87149822be28
Eugen Cutic <eugen_cutic@yahoo.com>
2018-11-18 01:57:26 +0200

    manage trip activity layout w/o right colors for seek/rating bar

---
+++ app/src/main/java/com/cutic/eugen/traveljournal/MainActivity.java
@@ -0,0 +1,17 @@
+package com.cutic.eugen.traveljournal;
+
+import android.drm.DrmStore;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.manage_trip_bar));
+    }
+}
